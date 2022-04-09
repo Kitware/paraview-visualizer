daa078c723982f2a63f273fc28d5e57dde310792
Earl Sioson <earlsioson@users.noreply.github.com>
2022-04-09 10:16:54 -0400

    chore(docker): update scripts with latest from cookiecutter

---
+++ docker/scripts/build_server.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+CURRENT_DIR=`dirname "$0"`
+
+# Since Mac doesn't come with realpath by default, let's set the full
+# paths using PWD.
+pushd .
+cd $CURRENT_DIR/..
+DEPLOY_DIR=$PWD
+cd $CURRENT_DIR/../../..
+ROOT_DIR=$PWD
+popd
+
+docker run --rm --gpus all \
+    -e TRAME_BUILD_ONLY=1 \
+    -e TRAME_PARAVIEW=/opt/paraview   \
+    -v "$TRAME_PARAVIEW:/opt/paraview" \
+    -v "$DEPLOY_DIR:/deploy" \
+    -v "$ROOT_DIR/paraview-visualizer:/local-app" \
+    kitware/trame:1.2-glvnd-runtime-ubuntu20.04-py39
--- docker/scripts/run_image.sh
+++ docker/scripts/run_image.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
-
-CURRENT_DIR=`dirname "$0"`
-
-cd $CURRENT_DIR/..
-DEPLOY_DIR=$PWD
-
-# To capture logs and look at them for debug
-# -v "$DEPLOY_DIR/server/logs:/deploy/server/logs" \
-
-docker run -it --rm --gpus all -p 8080:80 \
+docker run --rm --gpus all -p 8080:80 \
     -v "$HOME:/data" \
+    -d \
     pv-visualizer
