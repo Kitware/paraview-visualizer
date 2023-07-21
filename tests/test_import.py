2fecbeca93d204e8a7b2a77e937cba205b9318b6
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2023-07-21 15:36:52 -0600

    ci: fix test

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,6 +1,6 @@
 def test_import():
     try:
-        from pv_visualizer import main  # noqa: F401
+        from pv_visualizer.app import main  # noqa: F401
     except ModuleNotFoundError:
         # missing paraview
         pass
